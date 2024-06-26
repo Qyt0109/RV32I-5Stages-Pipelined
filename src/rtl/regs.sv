1617a2dd5fbc00a9e47ea46406c3be07670958fc
DaoQuyet <nmhtdyt@gmail.com>
2024-06-26 17:00:23 +0700

    no message

--- src/rtl/regs.sv
+++ src/rtl/regs.sv
@@ -1,5 +1,9 @@
+`define ZERO_REG_ADDR 0
+`define ZERO_REG_DATA 0
+
 module regs (
     input clk,
+    input rst,
 
     // region control by [STAGE 2 DECODE]
     input       rs_rd_en,  // source registers read enable
@@ -20,17 +24,20 @@ module regs (
   reg [31:0] x[1:31];  // regs from x1 to x31 (x0 is hard-wired with 0 value)
 
   // region read from x
-  `define ZERO_REG_ADDR 0
-  `define ZERO_REG_DATA 0
-
   reg [4:0] r_rs1;  // registered rs1
   reg [4:0] r_rs2;  // registered rs2
 
   // syn read with clk
-  always @(posedge clk) begin : sync_read_process
-    if (rs_rd_en) begin
-      r_rs1 <= rs1;  // update r_rs1 so the rs1_rdata = x[r_rs1] will also be updated
-      r_rs2 <= rs2;  // update r_rs2 so the rs2_rdata = x[r_rs2] will also be updated
+  always @(posedge clk, posedge rst) begin : sync_read_process
+    if (rst) begin
+      r_rs1 <= 0;
+      r_rs2 <= 0;
+    end else begin
+      // only read the register if [STAGE 2 DECODE] is enabled
+      if (rs_rd_en) begin
+        r_rs1 <= rs1;  // update r_rs1 so the rs1_rdata = x[r_rs1] will also be updated
+        r_rs2 <= rs2;  // update r_rs2 so the rs2_rdata = x[r_rs2] will also be updated
+      end
     end
   end
 
@@ -44,9 +51,17 @@ module regs (
   wire need_write = rd_wr_en && (rd != `ZERO_REG_ADDR);
 
   // syn write with clk and only write if [Stage 5] (Writeback) is preivously enable.
-  always @(posedge clk) begin : sync_write_process
-    // [Info] output of [Stage 5] (Writeback) is registered so delayed by 1 clk
-    if(need_write) x[rd] <= rd_wr_data;
+  integer i;
+  always @(posedge clk, posedge rst) begin : sync_write_process
+    if (rst) begin
+      // reset x
+      for (i = 1; i < 32; i = i + 1) begin
+        x[i] <= 0;
+      end
+    end else begin
+      // output of [Stage 5 WRITEBACK] is registered so delayed by 1 clk
+      if (need_write) x[rd] <= rd_wr_data;
+    end
   end
   // endregion write to x
 endmodule
